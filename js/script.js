fde0bfe3bbdcb3b5e965672ec4b21b15b309b28b
jazirehstudio2014-spec <jazirehstudio2014@gmail.com>
2026-03-22 19:29:09 +0100

    Create script.js

---
+++ js/script.js
@@ -0,0 +1,13 @@
+const categories = [
+  "Garden",
+  "Toys",
+  "Antiques",
+  "Comics",
+  "Sports",
+  "Electronics",
+  "Refurbished",
+  "Fashion",
+  "Motors",
+  "Collectibles",
+  "Home"
+];
